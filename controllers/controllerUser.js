00801e5551a279d2536362aaa0faf1aa42cdffc0
darrel2411 <tapsdarrel@gmail.com>
2021-08-21 12:41:58 +0800

    testing itinerary, user done & revisi controller user

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -5,50 +5,51 @@ const jwt = require("jsonwebtoken");
 const { validateEmail } = require("../helper/validation");
 
 class Controller {
-     static async register(req, res) {
-        let {email, password} = req.body;
-        if(!validateEmail(email)){
-            res.status(400).json('Email not Valid')
-            return; 
+    static async register(req, res) {
+        let { email, password } = req.body;
+        if (!validateEmail(email)) {
+            res.status(400).json("Email not Valid");
+            return;
         }
         const salt = bcrypt.genSaltSync(10);
-        password = bcrypt.hashSync(password, salt);  
-        try{
-            const response= await User.create({email, password})
-            console.log(response);
-            res.status(200).json(response);
-        }catch(error){
+        password = bcrypt.hashSync(password, salt);
+        try {
+            const response = await User.create({ email, password });
+            // console.log(response);
+            res.status(201).json(response);
+        } catch (error) {
             res.status(500).json(error);
         }
     }
     static async login(req, res) {
-        console.log("ENTER");
+        // console.log("ENTER");
         const { email, password } = req.body;
-        console.log(email,password);
-        try{
-            const response = await User.findOne({email})
-            if(!response){
-                res.status(400).json('Username and Password not match')
-            }else if(bcrypt.compareSync(password, response.password)){
-                let token = jwt.sign({id: response._id, email: response.email}, process.env.SECRET_KEY, {expiresIn: 60 * 60});
-                console.log(token);
-                res.status(200).json({token, email})
-            }else{
-                res.status(400).json('Username and Password not match')
+        // console.log(email, password);
+        try {
+            const response = await User.findOne({ email });
+            if (!response) {
+                // console.log(response);
+                res.status(404).json("Username and Password not match");
+            } else if (bcrypt.compareSync(password, response.password)) {
+                let token = jwt.sign({ id: response._id, email: response.email }, process.env.SECRET_KEY, { expiresIn: 60 * 60 });
+                // console.log(token);
+                res.status(200).json({ token, email });
+            } else {
+                res.status(400).json("Username and Password not match");
             }
-        }catch(error){
-            res.status(500).json(error)
+        } catch (error) {
+            res.status(500).json(error);
         }
     }
     static async getAll(req, res) {
-        console.log("ENTERING GETALL")
-        try{
-            const response = await User.find()
-            console.log(response);
+        // console.log("ENTERING GETALL");
+        try {
+            const response = await User.find();
+            // console.log(response);
             res.status(200).json(response);
-        }catch(error){
-            res.status(500).json(error)
-        }    
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 }
 
--- test/user.test.js
+++ test/user.test.js
@@ -1,21 +1,108 @@
-const mongoose = require('mongoose')
-const request = require('supertest')
-const app = require('../app.js')
-const User = require('../models/user');
-let user = {email: 'rafipratama@gmail.com', password: 'rafipratama', }
+// const mongoose = require("mongoose");
+// const request = require("supertest");
+// const app = require("../app.js");
+// const User = require("../models/user");
+// let user = { email: "rafipratama@gmail.com", password: "rafipratama" };
 
-const Admin = require('../Model')
+// const Admin = require("../Model");
 
-beforeAll(async () => {
-    mongoose.connect("mongodb://localhost:27017/test_database", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
+// beforeAll(async () => {
+//     mongoose.connect("mongodb://localhost:27017/test_database", {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//     });
+// });
 
-})
+// afterAll(async () => {
+//     await mongoose.connection.close()
+// })
 
-afterAll(async () => {
-    await mongoose.connection.close()
-})
+// test("should 1+1 equal 2", () => {
+//     expect(1 + 1).toEqual(2);
+// });
+
+const request = require("supertest");
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const app = require("../app");
+
+let access_token;
+
+describe("Test case user", () => {
+    beforeAll(async () => {
+        try {
+            await mongoose.connect("mongodb://localhost:27017/test", {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    afterAll(async () => {
+        const collections = await mongoose.connection.db.collections();
+        for (let collection of collections) {
+            await collection.deleteMany();
+        }
+        await mongoose.disconnect();
+    });
+
+    it("should be able to register a user", async () => {
+        const response = await request(app).post("/users/register").send({
+            email: "test@mail.com",
+            password: "test123",
+        });
+        expect(response.status).toBe(201);
+    });
+
+    it("email must be in valid standard", async () => {
+        const response = await request(app).post("/users/register").send({
+            email: "testail.com",
+            password: "test123",
+        });
+        expect(response.status).toBe(400);
+    });
+
+    it("Login successs", async () => {
+        const response = await request(app).post("/users/login").send({
+            email: "test@mail.com",
+            password: "test123",
+        });
+        // console.log(response);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("token", expect.any(String));
+        access_token = response.body.token;
+    });
+
+    it("Login failed because email/password wrong", async () => {
+        const response = await request(app).post("/users/login").send({
+            email: "test@mail.com",
+            password: "test12345",
+        });
+        // console.log(response);
+        expect(response.status).toBe(400);
+        expect(response.body).toBe("Username and Password not match");
+    });
+
+    it("Login failed because email not registered", async () => {
+        const response = await request(app).post("/users/login").send({
+            email: "test12345@mail.com",
+            password: "test123",
+        });
+        // console.log(response);
+        expect(response.status).toBe(404);
+        expect(response.body).toBe("Username and Password not match");
+    });
+
+    it("Get all user", async () => {
+        const response = await request(app).get("/users").set("access_token", access_token).send();
+        // console.log(response.body, 123123);
+        expect(response.status).toBe(200);
+        // expect(response.body).toBe("Username and Password not match");
+    });
+});
