44a28f1f751e8005ec2edd647176e0734229bb4c
darrel2411 <tapsdarrel@gmail.com>
2021-08-25 23:29:09 +0800

    revisi cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 // const { run } = require("./config");
 // const connectDB = require("./config");
 const router = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+app.use(cors());
 // connectDB();
 
 module.exports = app;
